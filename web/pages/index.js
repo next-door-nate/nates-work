17a8bd5a14abb0244d8031e0445624f5cb0ec956
Nate van der Vis <natevandervis@gmail.com>
2022-12-12 20:42:04 -0600

    ✨ init: create-next-app

---
+++ web/pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Nate van der Vis | Design 🤝 Engineering</title>
+        <meta name="description" content="Serving up a tastier web." />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <main>
+        <h1>Nates Work</h1>
+      </main>
+
+      <footer>
+        <p>&copy; 2022 - Ride or die</p>
+      </footer>
+    </div>
+  );
+}
