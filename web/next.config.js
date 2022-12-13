452ccf3999a22991491952b896e2aa549d6b969d
Nate van der Vis <natevandervis@gmail.com>
2022-12-13 12:27:46 -0600

    🚧 WIP: testing turbo cloudflare

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: "experimental-edge",
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- web/pages/api/hello.js
+++ web/pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export const config = {
+  runtime: "experimental-edge",
+};
+
+export default async function (req) {
+  return new Response(JSON.stringify({ name: "John Doe" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
