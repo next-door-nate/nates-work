efa559f32b33df351ba9cec9a41da976dc91b1d3
Nate van der Vis <natevandervis@gmail.com>
2023-03-13 16:41:10 -0500

    💅 update: fine tuning the cube

--- web/components/BannerHome.js
+++ web/components/BannerHome.js
@@ -9,6 +9,7 @@ import {
   OrbitControls,
   Environment,
   Lightformer,
+  Stage,
 } from "@react-three/drei";
 import { EffectComposer, DepthOfField } from "@react-three/postprocessing";
 
@@ -23,10 +24,11 @@ export default function BannerHome({ block }) {
         <Canvas
           shadows
           gl={{ antialias: false }}
-          camera={{ position: [-30, 35, -15], near: 30, far: 55, fov: 12 }}
+          camera={{ position: [-30, 35, -15], near: 30, far: 155, fov: 30 }}
         >
           <color attach="background" args={["#efffaf"]} />
           <ambientLight intensity={0.5} />
+
           <directionalLight
             position={[-10, 10, 5]}
             shadow-mapSize={[256, 256]}
@@ -40,22 +42,41 @@ export default function BannerHome({ block }) {
             <Lightformer position={[10, 0, -10]} scale={10} color="red" intensity={6} />
             <Lightformer position={[-10, -10, -10]} scale={10} intensity={4} />
           </Environment>
-          <mesh>
-            <boxGeometry args={[15, 15, 15]} />
-            <meshStandardMaterial />
-          </mesh>
+          <AccumulativeShadows
+            temporal
+            frames={Infinity}
+            alphaTest={1}
+            blend={200}
+            limit={1500}
+            scale={25}
+            position={[0, -0.05, 0]}
+          >
+            <RandomizedLight
+              amount={1}
+              mapSize={512}
+              radius={5}
+              ambient={0.5}
+              position={[-10, 10, 5]}
+              size={10}
+              bias={0.001}
+            />
+          </AccumulativeShadows>
+          <Stage
+            intensity={0.5}
+            environment="city"
+            shadows={{ type: "accumulative", bias: -0.001 }}
+            adjustCamera={false}
+          >
+            <mesh>
+              <boxGeometry args={[15, 15, 15]} />
+              <meshBasicMaterial color={"#e1f789"} wireframe />
+            </mesh>
+          </Stage>
 
           <EffectComposer>
-            <DepthOfField target={[0, 0, 0]} focusRange={0.55} bokehScale={8} />
+            <DepthOfField target={[0, 0, 0]} focusRange={1} bokehScale={0} />
           </EffectComposer>
-          <OrbitControls
-            autoRotate
-            autoRotateSpeed={1}
-            enablePan={false}
-            enableZoom={false}
-            minPolarAngle={Math.PI / 4}
-            maxPolarAngle={Math.PI / 4}
-          />
+          <OrbitControls enableZoom={false} autoRotate />
         </Canvas>
       </div>
     </section>
