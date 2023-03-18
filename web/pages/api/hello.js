9a09607078078167056e76936425998f1dffc812
Nate van der Vis <natevandervis@gmail.com>
2023-03-18 12:17:27 -0500

    🎈bug: cutting free experimental-edge runtime

--- web/pages/api/hello.js
+++ web/pages/api/hello.js
@@ -1,6 +1,6 @@
-export const config = {
-  runtime: "experimental-edge",
-};
+// export const config = {
+//   runtime: "experimental-edge",
+// };
 
 export default async function (req) {
   return new Response(JSON.stringify({ name: "John Doe" }), {
