97ad20031fe900c0bfb0f41a2bdb42598c85d239
Nate van der Vis <natevandervis@gmail.com>
2024-03-27 15:41:07 -0500

    🏗 building: making api endpoint experimental-edge

--- web/pages/api/songs.js
+++ web/pages/api/songs.js
@@ -1,4 +1,4 @@
-export const config = { runtime: "edge" };
+export const config = { runtime: "experimental-edge" };
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
