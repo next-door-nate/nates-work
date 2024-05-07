5afbd8ca99fe1910c6c8030b03efc518a78ebd61
next-door-nate <natevandervis@gmail.com>
2024-05-06 21:31:33 -0500

    💅 update: fixing syntax

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" type="image/png" href="/favicon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Pathway+Extreme:ital,opsz,wght@0,8..144,300;0,8..144,400;0,8..144,600;0,8..144,800;1,8..144,300;1,8..144,400;1,8..144,600;1,8..144,800&display=swap"
             rel="stylesheet"
