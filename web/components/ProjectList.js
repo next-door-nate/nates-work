403088c791ba61bb2c0a25ffdc1af571b2716333
next-door-nate <natevandervis@gmail.com>
2024-05-06 21:35:53 -0500

    💅 update: changing card style

--- web/components/ProjectList.js
+++ web/components/ProjectList.js
@@ -10,49 +10,56 @@ export default function ProjectList({ block }) {
       <Container>
         {block.projects.length > 0 && (
           <div className={styles.projects}>
-            {block.projects.map((project) => {
+            {block.projects.map((project, i) => {
               return (
                 <Link
-                  key={project._key}
-                  className={styles.card}
+                  className={styles.project}
+                  key={project._key + i}
                   href={linkResolver(project)}
                   title={project.title}
                 >
-                  <div key={project._key}>
-                    <div className={styles.background}>
-                      <OptimizedImage image={project.featured_image} blurHash={project.blurHash} />
-                    </div>
-                    <div className={styles.logo}>
-                      <OptimizedImage image={project.company.logo} />
-                    </div>
-                    <div className={styles.content}>
-                      <div className={styles.copy}>
-                        {project.company && (
-                          <h5 className={styles.company}>{project.company.name}</h5>
-                        )}
-                        {project.title && <h2>{project.title}</h2>}
-                        {project.tools.length > 0 && (
-                          <div className={styles.meta}>
-                            {project.roles.length > 0 && (
-                              <div>
-                                <h5>Role{project.roles.length > 1 && `s`}:</h5>
-                                {project.roles.map((role) => {
-                                  return <p key={role._key}>{role.title}</p>;
-                                })}
-                              </div>
-                            )}
-
-                            {project.tools.length > 0 && (
-                              <div>
-                                <h5>Tool{project.tools.length > 1 && `s`}:</h5>
-                                {project.tools.map((tool) => {
-                                  return <p key={tool._key}>{tool.title}</p>;
-                                })}
-                              </div>
-                            )}
-                          </div>
-                        )}
+                  <div className={styles.card}>
+                    <div className={styles.image}>
+                      <div className={styles.background}>
+                        <OptimizedImage
+                          image={project.featured_image}
+                          blurHash={project.blurHash}
+                        />
                       </div>
+                      <div className={styles.logo}>
+                        <OptimizedImage image={project.company.logo} />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className={styles.contents}>
+                    <div className={styles.copy}>
+                      {project.company && (
+                        <h5 className={styles.company}>
+                          {project.company.name}
+                        </h5>
+                      )}
+                      {project.title && <h2>{project.title}</h2>}
+                      {project.tools.length > 0 && (
+                        <div className={styles.meta}>
+                          {project.roles.length > 0 && (
+                            <div>
+                              <h5>Role{project.roles.length > 1 && `s`}:</h5>
+                              {project.roles.map((role) => {
+                                return <p key={role._key}>{role.title}</p>;
+                              })}
+                            </div>
+                          )}
+                          {project.tools.length > 0 && (
+                            <div>
+                              <h5>Tool{project.tools.length > 1 && `s`}:</h5>
+                              {project.tools.map((tool, i) => {
+                                return <p key={tool._key}>{tool.title}</p>;
+                              })}
+                            </div>
+                          )}
+                        </div>
+                      )}
                     </div>
                   </div>
                 </Link>
