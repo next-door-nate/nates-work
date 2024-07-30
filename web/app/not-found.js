2bc65b4715dad8637140464cd2e46bb958c1f370
next-door-nate <natevandervis@gmail.com>
2024-07-30 11:47:04 -0500

    🏗️ building: adding app directory not-found file

---
+++ web/app/not-found.js
@@ -0,0 +1,11 @@
+export const runtime = 'edge';
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
+}
